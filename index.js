a8d7db9b40fac13138e744bcf8fa64405ad8434d
Jose <jlurquieta1@gmail.com>
2024-06-07 19:09:50 -0600

    f5

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ const cors = require('cors');
 const multer = require('multer');
 const mysql = require('mysql');
 const dotenv = require("dotenv");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
 const authenticateToken = require('./authInterceptor'); // Importar el middleware
 
-
-dotenv.config({path: './db.env'})
+dotenv.config({ path: './db.env' });
 
 const app = express();
 
 app.use(bodyParser.json());
-
-
 app.use(cors());
 
 const port = process.env.PORT || 3000;
@@ -25,18 +22,157 @@ const dbConfig = {
   user: process.env.user,
   password: process.env.password,
   database: process.env.database,
+  connectionLimit: 10, // Adjust based on your needs
 };
 
-const connection = mysql.createConnection(dbConfig);
+const pool = mysql.createPool(dbConfig);
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error de conexión a la base de datos: ", err);
-    return;
-  }
-  console.log("Conexión a la base de datos exitosa");
+pool.on('connection', (connection) => {
+  console.log('New connection established with ID:', connection.threadId);
 });
 
+pool.on('acquire', (connection) => {
+  console.log('Connection %d acquired', connection.threadId);
+});
+
+pool.on('release', (connection) => {
+  console.log('Connection %d released', connection.threadId);
+});
+
+pool.on('error', (err) => {
+  console.error('MySQL error: ', err);
+});
+
+function handleDisconnect() {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection: ', err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      connection.release();
+      console.log('MySQL connected');
+    }
+  });
+}
+
+handleDisconnect();
+
+//login de un usuario
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
+        return res.status(401).send('Nombre de usuario o contraseña incorrecta');
+      }
+      if (results[0].role !== 'user') {
+        return res.status(403).send('Acceso denegado');
+      }
+      const token = jwt.sign({ id: results[0].id, role: results[0].role }, 'secretkey', { expiresIn: '8h' });
+      res.status(200).send({ token });
+    });
+  });
+});
+
+//hacer login de un admin 
+app.post('/admin', (req, res) => {
+  const { username, password } = req.body;
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
+        return res.status(401).send('Nombre de usuario o contraseña incorrecta');
+      }
+      if (results[0].role !== 'admin') {
+        return res.status(403).send('Acceso denegado');
+      }
+      const token = jwt.sign({ id: results[0].id, role: results[0].role }, 'secretkey', { expiresIn: '8h' });
+      res.status(200).send({ token });
+    });
+  });
+});
+
+//Obtener inventario por usuario
+app.get('/inventory/:username', (req, res) => {
+  const username = req.params.username;
+  const userTableName = `inventory_${username}`;
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query(`SELECT * FROM ${userTableName}`, (err, results) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      res.status(200).json(results);
+    });
+  });
+});
+
+//Agregar item a usuario
+app.post('/inventory/:username', (req, res) => {
+  const username = req.params.username;
+  const { item_name, quantity } = req.body;
+  const userTableName = `inventory_${username}`;
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query(`INSERT INTO ${userTableName} (item_name, quantity) VALUES (?, ?)`, [item_name, quantity], (err) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      res.status(201).send('Item added to inventory');
+    });
+  });
+});
+
+app.put('/inventory/:id', authenticateToken, (req, res) => {
+  const { item_name, quantity } = req.body;
+  const userId = req.user.id;
+  const itemId = req.params.id;
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query('UPDATE inventories SET item_name = ?, quantity = ? WHERE id = ? AND user_id = ?', [item_name, quantity, itemId, userId], (err) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      res.status(200).send('Item correctamente editado');
+    });
+  });
+});
+
+app.delete('/inventory/:id', authenticateToken, (req, res) => {
+  const userId = req.user.id;
+  const itemId = req.params.id;
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query('DELETE FROM inventories WHERE id = ? AND user_id = ?', [itemId, userId], (err) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      res.status(200).send('Item correctamente eliminado');
+    });
+  });
+});
+
+// Endpoint para obtener los usuarios
+app.get('/users', (req, res) => {
+  const sql = "SELECT id, username, role FROM users";
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query(sql, (err, results) => {
+      connection.release();
+      if (err) {
+        console.error("Error al obtener datos de la base de datos: ", err);
+        res.status(500).send({ error: "Error al obtener datos de la base de datos" });
+      } else {
+        res.send(results);
+      }
+    });
+  });
+});
 
 // Registro de administradores
 app.post('/register/admin', async (req, res) => {
@@ -44,16 +180,35 @@ app.post('/register/admin', async (req, res) => {
   const role = 'admin';
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  connection.beginTransaction(err => {
+  pool.getConnection((err, connection) => {
     if (err) return res.status(500).send(err);
 
-    connection.query('INSERT INTO users (username, password, role) VALUES (?, ?, ?)', [username, hashedPassword, 'admin'], (err, result) => {
+    connection.beginTransaction(err => {
       if (err) {
-        connection.rollback(() => {
-          return res.status(500).send(err);
-        });
-      } else {
+        connection.release();
+        return res.status(500).send(err);
       }
+
+      connection.query('INSERT INTO users (username, password, role) VALUES (?, ?, ?)', [username, hashedPassword, 'admin'], (err, result) => {
+        if (err) {
+          connection.rollback(() => {
+            connection.release();
+            return res.status(500).send(err);
+          });
+        } else {
+          connection.commit(err => {
+            if (err) {
+              connection.rollback(() => {
+                connection.release();
+                return res.status(500).send(err);
+              });
+            } else {
+              connection.release();
+              res.status(201).send('Administrador registrado correctamente');
+            }
+          });
+        }
+      });
     });
   });
 });
@@ -64,138 +219,59 @@ app.post('/register/user', async (req, res) => {
   const role = 'user';
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  connection.beginTransaction(err => {
+  pool.getConnection((err, connection) => {
     if (err) return res.status(500).send(err);
 
-    connection.query('INSERT INTO users (username, password, role) VALUES (?, ?, ?)', [username, hashedPassword, 'user'], (err, result) => {
+    connection.beginTransaction(err => {
       if (err) {
-        connection.rollback(() => {
-          return res.status(500).send(err);
-        });
-      } else {
-        const userTableName = `inventory_${username}`;
-
-        connection.query(`CREATE TABLE ${userTableName} LIKE inventories`, (err) => {
-          if (err) {
-            connection.rollback(() => {
-              return res.status(500).send(err);
-            });
-          } else {
-            connection.query(`INSERT INTO ${userTableName} (item_name, quantity) SELECT item_name, quantity FROM inventories`, (err) => {
-              if (err) {
-                connection.rollback(() => {
-                  return res.status(500).send(err);
-                });
-              } else {
-                connection.commit(err => {
-                  if (err) {
-                    connection.rollback(() => {
-                      return res.status(500).send(err);
-                    });
-                  } else {
-                    res.status(201).send('Usuario correctamente registrado');
-                  }
-                });
-              }
-            });
-          }
-        });
+        connection.release();
+        return res.status(500).send(err);
       }
+
+      connection.query('INSERT INTO users (username, password, role) VALUES (?, ?, ?)', [username, hashedPassword, 'user'], (err, result) => {
+        if (err) {
+          connection.rollback(() => {
+            connection.release();
+            return res.status(500).send(err);
+          });
+        } else {
+          const userTableName = `inventory_${username}`;
+
+          connection.query(`CREATE TABLE ${userTableName} LIKE inventories`, (err) => {
+            if (err) {
+              connection.rollback(() => {
+                connection.release();
+                return res.status(500).send(err);
+              });
+            } else {
+              connection.query(`INSERT INTO ${userTableName} (item_name, quantity) SELECT item_name, quantity FROM inventories`, (err) => {
+                if (err) {
+                  connection.rollback(() => {
+                    connection.release();
+                    return res.status(500).send(err);
+                  });
+                } else {
+                  connection.commit(err => {
+                    if (err) {
+                      connection.rollback(() => {
+                        connection.release();
+                        return res.status(500).send(err);
+                      });
+                    } else {
+                      connection.release();
+                      res.status(201).send('Usuario registrado correctamente');
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
     });
   });
 });
-//hacer login de un usuario
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
 
-  connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
-    if (err) return res.status(500).send(err);
-    if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
-      return res.status(401).send('Nombre de usuario o contraseña incorrecta');
-    }
-    if (results[0].role !== 'user') {
-      return res.status(403).send('Acceso denegado');
-    }
-    const token = jwt.sign({ id: results[0].id, role: results[0].role }, 'secretkey', { expiresIn: '8h' });
-    res.status(200).send({ token });
-  });
-});
-
-//hacer login de un admin 
-app.post('/admin', (req, res) => {
-  const { username, password } = req.body;
-
-  connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
-    if (err) return res.status(500).send(err);
-    if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
-      return res.status(401).send('Nombre de usuario o contraseña incorrecta');
-    }
-    if (results[0].role !== 'admin') {
-      return res.status(403).send('Acceso denegado');
-    }
-    const token = jwt.sign({ id: results[0].id, role: results[0].role }, 'secretkey', { expiresIn: '8h' });
-    res.status(200).send({ token });
-  });
-});
-
-
-//Obtener inventario por usuario
-app.get('/inventory/:username', (req, res) => {
-  const username = req.params.username;
-  const userTableName = `inventory_${username}`;
-
-  connection.query(`SELECT * FROM ${userTableName}`, (err, results) => {
-    if (err) return res.status(500).send(err);
-    res.status(200).json(results);
-  });
-});
-
-//Agregar item a usuario
-app.post('/inventory/:username', (req, res) => {
-  const username = req.params.username;
-  const { item_name, quantity } = req.body;
-  const userTableName = `inventory_${username}`;
-
-  connection.query(`INSERT INTO ${userTableName} (item_name, quantity) VALUES (?, ?)`, [item_name, quantity], (err) => {
-    if (err) return res.status(500).send(err);
-    res.status(201).send('Item added to inventory');
-  });
-});
-
-
-
-app.put('/inventory/:id', authenticateToken, (req, res) => {
-  const { item_name, quantity } = req.body;
-  const userId = req.user.id;
-  const itemId = req.params.id;
-  connection.query('UPDATE inventories SET item_name = ?, quantity = ? WHERE id = ? AND user_id = ?', [item_name, quantity, itemId, userId], (err) => {
-    if (err) return res.status(500).send(err);
-    res.status(200).send('Item correctamente editado');
-  });
-});
-
-app.delete('/inventory/:id', authenticateToken, (req, res) => {
-  const userId = req.user.id;
-  const itemId = req.params.id;
-  connection.query('DELETE FROM inventories WHERE id = ? AND user_id = ?', [itemId, userId], (err) => {
-    if (err) return res.status(500).send(err);
-    res.status(200).send('Item correctamente eliminado');
-  });
-});
-
-// Endpoint para obtener los usuarios
-app.get('/users', (req, res) => {
-  const sql = "SELECT id, username, role FROM users";
-  connection.query(sql, (err, results) => {
-    if (err) {
-      console.error("Error al obtener datos de la base de datos: ", err);
-      res.status(500).send({ error: "Error al obtener datos de la base de datos" });
-    } else {
-      res.send(results);
-    }
-  });
-});
-
-app.listen(3000 , () => {
-  console.log('Server running on port 3000');
+app.listen(port, () => {
+  console.log(`Servidor ejecutándose en el puerto ${port}`);
 });
