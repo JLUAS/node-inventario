29643e7c583bc272605946ef6a0c47556d605b09
Jose <jlurquieta1@gmail.com>
2024-06-13 17:30:43 -0600

    File Upload

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 const authenticateToken = require('./authInterceptor'); // Importar el middleware
-const XlsxPopulate = require('xlsx-populate');
-const path = require('path');
-const fs = require('fs');
-
+const XlsxPopulate = require('xlsx-populate')
 dotenv.config({ path: './db.env' });
 
 const app = express();
@@ -125,6 +122,435 @@ app.post('/upload/excel', upload.single('myFile'), async (req, res) => {
   }
 });
 
+app.post('/upload/excel', upload.single('myFile'), (req, res) => {
+  const filePath = req.file.path;
+  try {
+      const workbook = XlsxPopulate.fromFileAsync(filePath);
+      const sheet = workbook.sheet(0);
+      const usedRange = sheet.usedRange();
+      const data = usedRange.value();
+      const headers = data[0].map(header => `\`${header}\``); // Asegurarse de usar backticks para los nombres de columnas
+
+      // Eliminar todos los registros existentes en la tabla 'data'
+     new Promise((resolve, reject) => {
+        connection.query('DELETE FROM data', (err, result) => {
+          if (err) {
+            console.error('Error deleting existing records:', err);
+            reject(err);
+          } else {
+            console.log('Existing records deleted');
+            resolve(result);
+          }
+        });
+      });
+
+      // Inserta cada fila de datos, omitiendo la primera fila que contiene los encabezados
+      for (let i = 1; i < data.length; i++) {
+        const row = data[i];
+        const query = `INSERT INTO data (${headers.join(", ")}) VALUES (${row.map(() => "?").join(", ")})`;
+       new Promise((resolve, reject) => {
+          connection.query(query, row, (err, result) => {
+            if (err) {
+              console.error(`Error inserting row ${i}:`, err);
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+      }
+
+      console.log('File processed successfully');
+      res.send({ data: "File processed successfully" });
+    } catch (error) {
+      console.error('Error processing file:', error);
+      res.status(500).send({ error: "Error processing file" });
+    } finally {
+      // Elimina el archivo temporal después de procesarlo
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error('Error deleting temp file:', err);
+        }
+      });
+    }
+});
+
+// Login de un usuario
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
+        return res.status(401).send('Nombre de usuario o contraseña incorrecta');
+      }
+      if (results[0].role !== 'user') {
+        return res.status(403).send('Acceso denegado');
+      }
+      const token = jwt.sign({ id: results[0].id, role: results[0].role }, 'secretkey', { expiresIn: '8h' });
+      res.status(200).send({ token });
+    });
+  });
+});
+
+// Hacer login de un admin 
+app.post('/admin', (req, res) => {
+  const { username, password } = req.body;
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
+        return res.status(401).send('Nombre de usuario o contraseña incorrecta');
+      }
+      if (results[0].role !== 'admin') {
+        return res.status(403).send('Acceso denegado');
+      }
+      const token = jwt.sign({ id: results[0].id, role: results[0].role }, 'secretkey', { expiresIn: '8h' });
+      res.status(200).send({ token });
+    });
+  });
+});
+
+// Obtener inventario general
+app.get('/inventory/main', (req, res) => {
+  const sql = `SELECT * FROM data`;
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query(sql, (err, results) => {
+      connection.release();
+      if (err) {
+        console.error("Error al obtener datos de la base de datos principal: ", err);
+        res.status(500).send({ error: "Error al obtener datos de la base de datos" });
+      } else {
+        res.send(results);
+      }
+    });
+  });
+});
+
+// Obtener inventario por usuario
+app.get('/inventory/:username', (req, res) => {
+  const username = req.params.username;
+  const userTableName = `inventory_${username}`;
+  
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error al obtener conexión de la base de datos: ", err);
+      return res.status(500).send({ error: "Error al obtener conexión de la base de datos" });
+    }
+
+    // Crear la nueva tabla con la misma estructura que 'data'
+    connection.query(`DROP TABLE IF EXISTS ??`, [userTableName], (err) => {
+      if (err) {
+        connection.release();
+        console.error("Error al eliminar la tabla: ", err);
+        return res.status(500).send({ error: "Error al eliminar la tabla" });
+      }
+      
+      connection.query(`CREATE TABLE ?? LIKE data`, [userTableName], (err) => {
+        if (err) {
+          connection.release();
+          console.error("Error al crear la tabla: ", err);
+          return res.status(500).send({ error: "Error al crear la tabla" });
+        }
+
+        // Insertar los datos de 'data' en la nueva tabla
+        connection.query(`INSERT INTO ?? SELECT * FROM data`, [userTableName], (err) => {
+          if (err) {
+            connection.release();
+            console.error("Error al copiar los datos a la nueva tabla: ", err);
+            return res.status(500).send({ error: "Error al copiar los datos a la nueva tabla" });
+          }
+          
+          // Seleccionar los datos de la nueva tabla
+          const sql = `SELECT * FROM ??`;
+          connection.query(sql, [userTableName], (err, results) => {
+            connection.release();
+            if (err) {
+              console.error("Error al obtener datos de la base de datos: ", err);
+              res.status(500).send({ error: "Error al obtener datos de la base de datos" });
+            } else {
+              res.send(results);
+            }
+          });
+        });
+      });
+    });
+  });
+});
+
+
+// Agregar item a tabla de usuario
+app.post('/inventory/:username', (req, res) => {
+  const username = req.params.username;
+  const { item_name, quantity } = req.body;
+  const userTableName = `inventory_${username}`;
+  pool.getConnection((err, connection) => {
+    connection.query(`INSERT INTO ${userTableName} (item_name, quantity) VALUES (?, ?)`, [item_name, quantity], (err) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      res.status(201).send('Item added to inventory');
+    });
+  });
+});
+
+// Agregar item a base principal
+app.post('/inventory', (req, res) => {
+  const { item_name, quantity } = req.body;
+
+  if (!item_name || !quantity) {
+    console.error('Validation Error: Item name and quantity are required');
+    return res.status(400).send('Item name and quantity are required');
+  }
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Database Connection Error:', err);
+      return res.status(500).send('Database Connection Error');
+    }
+
+    connection.query('SELECT quantity FROM data WHERE item_name = ?', [item_name], (err, results) => {
+      if (err) {
+        connection.release();
+        console.error('Database Query Error:', err);
+        return res.status(500).send('Database Query Error');
+      }
+
+      if (results.length > 0) {
+        // Item exists, update quantity
+        const newQuantity = results[0].quantity + quantity;
+        connection.query('UPDATE data SET quantity = ? WHERE item_name = ?', [newQuantity, item_name], (err) => {
+          connection.release();
+          if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send('Error executing query');
+          }
+          res.status(200).send('Item quantity updated');
+        });
+      } else {
+        // Item does not exist, insert new record
+        connection.query('INSERT INTO data (item_name, quantity) VALUES (?, ?)', [item_name, quantity], (err) => {
+          connection.release();
+          if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send('Error executing query');
+          }
+          res.status(201).send('Item added to inventory');
+        });
+      }
+    });
+  });
+});
+
+app.put('/inventory/:id', authenticateToken, (req, res) => {
+  const { item_name, quantity } = req.body;
+  const userId = req.user.id;
+  const itemId = req.params.id;
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query('UPDATE data SET item_name = ?, quantity = ? WHERE id = ? AND user_id = ?', [item_name, quantity, itemId, userId], (err) => {
+      connection.release();
+      if (err) return res.status(500).send(err);
+      res.status(200).send('Item correctamente editado');
+    });
+  });
+});
+
+app.delete('/inventory/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    console.error('Validation Error: ID is required');
+    return res.status(400).send('ID is required');
+  }
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Database Connection Error:', err);
+      return res.status(500).send('Database Connection Error');
+    }
+
+    connection.query('DELETE FROM data WHERE id = ?', [id], (err, results) => {
+      connection.release();
+      if (err) {
+        console.error('Error executing query:', err);
+        return res.status(500).send('Error executing query');
+      }
+
+      if (results.affectedRows === 0) {
+        return res.status(404).send('Item not found');
+      }
+
+      res.status(200).send('Item deleted');
+    });
+  });
+});
+
+// Endpoint para obtener los usuarios
+app.get('/users', (req, res) => {
+  const sql = `SELECT id, username, role FROM users`;
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+    connection.query(sql, (err, results) => {
+      connection.release();
+      if (err) {
+        console.error("Error al obtener datos de la base de datos: ", err);
+        res.status(500).send({ error: "Error al obtener datos de la base de datos" });
+      } else {
+        res.send(results);
+      }
+    });
+  });
+});
+
+// Registro de administradores
+app.post('/register/admin', async (req, res) => {
+  const { username, password } = req.body;
+  const role = 'admin';
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+
+    connection.beginTransaction(err => {
+      if (err) {
+        connection.release();
+        return res.status(500).send(err);
+      }
+
+      connection.query('INSERT INTO users (username, password, role) VALUES (?, ?, ?)', [username, hashedPassword, 'admin'], (err, result) => {
+        if (err) {
+          connection.rollback(() => {
+            connection.release();
+            return res.status(500).send(err);
+          });
+        } else {
+          connection.commit(err => {
+            if (err) {
+              connection.rollback(() => {
+                connection.release();
+                return res.status(500).send(err);
+              });
+            } else {
+              connection.release();
+              res.status(201).send('Administrador registrado correctamente');
+            }
+          });
+        }
+      });
+    });
+  });
+});
+
+// Registro de usuarios
+app.post('/register/user', async (req, res) => {
+  const { username, password } = req.body;
+  const role = 'user';
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  pool.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
+
+    connection.beginTransaction(err => {
+      if (err) {
+        connection.release();
+        return res.status(500).send(err);
+      }
+
+      connection.query('INSERT INTO users (username, password, role) VALUES (?, ?, ?)', [username, hashedPassword, 'user'], (err, result) => {
+        if (err) {
+          connection.rollback(() => {
+            connection.release();
+            return res.status(500).send(err);
+          });
+        } else {
+          const userTableName = `inventory_${username}`;
+
+          connection.query(`CREATE TABLE ${userTableName} LIKE data`, (err) => {
+            if (err) {
+              connection.rollback(() => {
+                connection.release();
+                return res.status(500).send(err);
+              });
+            } else {
+              connection.query(`INSERT INTO ${userTableName} (item_name, quantity) SELECT item_name, quantity FROM data`, (err) => {
+                if (err) {
+                  connection.rollback(() => {
+                    connection.release();
+                    return res.status(500).send(err);
+                  });
+                } else {
+                  connection.commit(err => {
+                    if (err) {
+                      connection.rollback(() => {
+                        connection.release();
+                        return res.status(500).send(err);
+                      });
+                    } else {
+                      connection.release();
+                      res.status(201).send('Usuario registrado correctamente');
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    });
+  });
+});
+
+//hacer post de una base de datos
+async function main() {
+  try {
+    const workbook = await XlsxPopulate.fromFileAsync('./Database.xlsx');
+    const sheet = workbook.sheet(0);
+    const usedRange = sheet.usedRange();
+    const data = usedRange.value();
+    const headers = data[0].map(header => `\`${header}\``); // Asegurarse de usar backticks para los nombres de columnas
+
+    // Eliminar todos los registros existentes en la tabla 'data'
+    await new Promise((resolve, reject) => {
+      pool.query('DELETE FROM data', (err, result) => {
+        if (err) {
+          console.error('Error deleting existing records:', err);
+          reject(err);
+        } else {
+          console.log('Existing records deleted');
+          resolve(result);
+        }
+      });
+    });
+
+    // Inserta cada fila de datos, omitiendo la primera fila que contiene los encabezados
+    for (let i = 1; i < data.length; i++) {
+      const row = data[i];
+      const query = `INSERT INTO data (${headers.join(", ")}) VALUES (${row.map(() => "?").join(", ")})`;
+      await new Promise((resolve, reject) => {
+        pool.query(query, row, (err, result) => {
+          if (err) {
+            console.error(`Error inserting row ${i}:`, err);
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+    }
+  } catch (error) {
+    console.error('Error processing file:', error);
+    throw error;
+  }
+}
+
 app.listen(port, () => {
   console.log(`Servidor ejecutándose en el puerto ${port}`);
+  main()
 });
